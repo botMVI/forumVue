14e3439398dd16fc15814923b7649e2b33fadace
Mike <ilmivy1024@gmail.com>
2020-07-16 17:11:38 +0600

    added from work

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Forum For Work</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          flat
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Войти', icon: 'lock', url: '/login'},
+        {title: 'Регистрация', icon: 'face', url: '/registration'},
+        {title: 'Сообщения', icon: 'bookmark_border', url: '/orders'},
+        {title: 'Новые сообщения', icon: 'note_add', url: '/new'},
+        {title: 'Мои сообщения', icon: 'list', url: '/list'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
